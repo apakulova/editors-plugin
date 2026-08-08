2130d0fcc5e4b969216f031fdbdd456c5750d501
Anna Akulova <redschoolgirl@gmail.com>
2026-08-08 22:35:33 +0300

    Load visual redesign stylesheet

--- landing/script.js
+++ landing/script.js
@@ -1,3 +1,8 @@
+const visualRedesign = document.createElement("link");
+visualRedesign.rel = "stylesheet";
+visualRedesign.href = "./visual-redesign.css?v=20260808-1";
+document.head.appendChild(visualRedesign);
+
 document.querySelectorAll("[data-report-row]").forEach((row) => {
   row.addEventListener("click", () => {
     document.querySelectorAll("[data-report-row]").forEach((item) => {
